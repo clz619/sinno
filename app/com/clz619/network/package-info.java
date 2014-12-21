d9351f1225fe9495483392e5e6555d48995abc96
clz619 <121733807@qq.com>
2014-12-21 14:57:21 +0800

    add network package

---
+++ app/com/clz619/network/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @project sinno
+ * @title package-info.java
+ * @author lizhong.chen
+ * @data 2014年12月21日下午2:56:52
+ * @description java network
+ * @tag 
+ * @version V1.0
+ */
+package com.clz619.network;
